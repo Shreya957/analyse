a7b72041621b3415274aaa511d0a484ee46beb7e
Shreyanka <shreya957@gmail.com>
2021-03-01 12:51:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+
+const express = require('express');
+const AWS = require('aws-sdk')
+const app = express();
+const path = require('path');
+const router = express.Router();
+var { ComprehendClient, BatchDetectEntitiesCommand, BatchDetectSentimentCommand,BatchDetectKeyPhrasesCommand ,DetectPiiEntitiesCommand} = require("@aws-sdk/client-comprehend");
+const comprehend = new ComprehendClient({ region: "us-east-2" });
+
+
+
+const bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+app.post('/analyze', async function (req, res) {
+  console.log(req.body)
+  let incomingTxt = req.body.text;
+  const params = {
+    TextList: [incomingTxt],
+    LanguageCode: 'en',
+    EndpointArn: 'arn:aws:iam::892613423932:user/ShreyaLaptop'
+  };
+
+  const paramsDE = {
+    Text: incomingTxt,
+    LanguageCode: 'en',
+    EndpointArn: 'arn:aws:iam::892613423932:user/ShreyaLaptop'
+  };
+  const detectEntity = new BatchDetectEntitiesCommand(
+    params
+  );
+  const detectSentiments = new BatchDetectSentimentCommand(
+    params
+  );
+  const detectKeyphrase = new BatchDetectKeyPhrasesCommand(
+    params
+  );
+const detectPii = new DetectPiiEntitiesCommand(
+  paramsDE
+  );
+  
+  let output = await comprehend.send(detectEntity);
+  let outputSentiments = await comprehend.send(detectSentiments); 
+  let keyphrase = await comprehend.send(detectKeyphrase);
+  let pii = await comprehend.send(detectPii);
+  //console.log(JSON.stringify(keyphrase.ResultList[0].KeyPhrases));
+  //console.log(JSON.stringify(output.ResultList[0].Entities));
+  // console.log(JSON.stringify(outputSentiments.ResultList[0]));
+  console.log(JSON.stringify(pii.Entities));
+
+  let analyze = { keyPhrases:keyphrase.ResultList[0].KeyPhrases,entities: output.ResultList[0].Entities, sentiment: outputSentiments.ResultList[0], pii: pii.Entities}
+  console.log(analyze)
+  res.send(analyze);
+})
+console.log("Server Started")
+app.listen(8000)
---
+++ public/awsComp1.js
@@ -0,0 +1,144 @@
+async function CreateTableFromJSON() {
+    try {
+
+        let tables = document.getElementsByTagName("table");
+        console.log(tables)
+        for (item of tables) { 
+            console.log(item); 
+           // item.remove();
+        } 
+      
+    } catch (err) {
+
+        console.log(err)
+    }
+    let entities = '';
+    var settings = {
+        "url": " http://localhost:8000/analyze",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({ "text": document.getElementById("w3review").value.trim() }),
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        var col = [];
+        var col1 = [];
+        let myBooks = response.entities;
+        let keyPhrases = response.keyPhrases;
+        //let pii = response.pii;
+        // Modify array based on incident tickets
+        myBooks.forEach(function(obj) {
+            if (obj.Text.startsWith("INF")) {
+                obj.Type = 'INCIDENT';
+                // Or: `obj.baz = [11, 22, 33];`
+            }
+            if (obj.Text.includes("Whirlpool")) {
+                obj.Type = 'ORGANISATION';
+                // Or: `obj.baz = [11, 22, 33];`
+            }
+        });
+        //alert(JSON.stringify(response.sentiment))
+        document.getElementById("card").style.display = "block";
+        let sentiment = response.sentiment.Sentiment;
+        document.getElementById("mood").innerHTML = sentiment;
+        if (sentiment === "NEUTRAL") {
+            document.getElementById('moodImage').src = 'neutral.png'
+        }
+        else if (sentiment === "POSITIVE") {
+            document.getElementById('moodImage').src = 'smile.png'
+
+        }
+        else if (sentiment === "NEGATIVE") {
+            document.getElementById('moodImage').src = 'sad.png'
+
+        } else {
+            document.getElementById('moodImage').src = 'neutral.svg'
+
+        }
+        // document.getElementById("moodDetails").innerHTML = JSON.stringify(response.sentiment.SentimentScore);
+
+        //document.createElement("div").innerHTML=response.sentiment;
+        for (var i = 0; i < myBooks.length; i++) {
+            for (var key in myBooks[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
+
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+
+        for (var i = 3; i < 5; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < myBooks.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 3; j < 5; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = myBooks[i][col[j]];              
+            }
+        }
+        //////////////////////////////////////////////////////////
+
+        for (var i = 0; i < keyPhrases.length; i++) {
+            for (var key in keyPhrases[i]) {
+                if (col1.indexOf(key) === -1) {
+                    col1.push(key);
+                }
+            }
+        }
+        // CREATE DYNAMIC TABLE.
+        var table1 = document.createElement("table");
+
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = table1.insertRow(-1);                   // TABLE ROW.
+
+
+        for (var i = 0; i < 2; i++) {
+            var th = document.createElement("th");
+            if (i == 0) {
+                th.innerHTML = "Confidence";
+            } else if (i == 1) {
+                th.innerHTML = "Keyphrase";
+            }  // TABLE HEADER.
+
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < keyPhrases.length; i++) {
+
+            tr = table1.insertRow(-1);
+
+            for (var j = 2; j < 4; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = keyPhrases[i][col1[j]];
+            }
+        }
+
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer = document.getElementById("showData");
+
+        divContainer.appendChild(table);
+        divContainer.appendChild(table1);
+
+    });
+
+
+}
+
